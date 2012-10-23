ff615e59f202ebb3469a3da7620708013a752a84
Justin Ridgewell <jridgewe@stetson.edu>
2012-10-23 02:56:14 -0400

    Don't let our prototype extensions be enumerable

--- libs/misc.js
+++ libs/misc.js
@@ -1,55 +1,50 @@
-Array.prototype.remove = function(e) {
+var DONTENUMERATE = function(Obj, property, val) {
+	Object.defineProperty(Obj, property, {
+		value: val,
+		configurable: true,
+		enumerable: false,
+		writable: true
+	});
+}
+
+DONTENUMERATE(Array.prototype, 'remove', function(e) {
 	for (var i = 0; i < this.length; ++i)
 		if (e == this[i]) return this.splice(i, 1);
-};
+});
 
-Array.prototype.each = function(fn) {
+DONTENUMERATE(Array.prototype, 'each', function(fn) {
 	for (var i = 0; i < this.length; ++i) fn(this[i]);
-};
+	return this;
+});
 
-Array.prototype.compact = function(fn) {
-	for (var i = 0; i < this.length; ++i)
+DONTENUMERATE(Array.prototype, 'compact', function(fn) {
+	for (var i = this.length - 1; i >= 0; --i)
 		if (this[i] == null) {
 			this.splice(i, 1);
 		}
 	return this;
-};
+});
 
-Array.prototype.clone = function() {
+DONTENUMERATE(Array.prototype, 'clone', function() {
 	return Array.prototype.slice.call(this);
-}
+});
 
-Function.prototype.clone = function() {
-    var that = this;
-    var temp = function temporary() { return that.apply(this, arguments); };
-    for( prop in this ) {
-        temp[prop] = this[prop];
-    }
-    return temp;
-};
-
-Object.defineProperty(Object.prototype, 'extend', {
-	value: function(source) {
-		for (var prop in source) {
-			if (prop != 'constructor') {
-				var s = source[prop];
-				if (typeof s == 'function') {
+DONTENUMERATE(Object.prototype, 'extend', function(source) {
+	$this = this;
+	for (var prop in source) {
+		if (prop != 'constructor') {
+			var s = source[prop];
+			if (typeof s != 'object') {
+				this[prop] = s;
+			} else {
+				if ('clone' in s && typeof s.clone == 'function') {
 					this[prop] = s.clone();
-				} else if (typeof s != 'object') {
-					this[prop] = s;
 				} else {
-					if ('clone' in s && typeof s.clone == 'function') {
-						this[prop] = s.clone();
-					} else {
-						// There are references to the Parent's objects
-						this[prop] = s;
-					}
+					// There are references to the Parent's objects
+					this[prop] = s;
 				}
 			}
 		}
-		return this;
-	},
-	configurable: true,
-	enumerable: false,
-	writable: true
+	}
+	return this;
 });
