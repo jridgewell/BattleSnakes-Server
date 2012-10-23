5ea469aee182c669eea61c3851f555a7c0205beb
Justin Ridgewell <jridgewe@stetson.edu>
2012-10-23 01:35:07 -0400

    Remove unnecessary type in Environment

--- libs/all/Environment/Environment.js
+++ libs/all/Environment/Environment.js
@@ -3,7 +3,6 @@ var GameObject = require('../GameObject');
 function Environment(){}
 
 Environment.prototype.extend(GameObject.prototype).extend({
-	type : '', //String of what it is ex: "Tree" "Rock"
 	sprite : '' // path to sprite img to load
 });
 
