a39e838deb95834f8ddc540b36c83b0f0cfcf657
Justin Ridgewell <jridgewe@stetson.edu>
2012-11-11 20:58:23 -0500

    Move Environment.collision() to GameObject

--- libs/all/Environment/Environment.js
+++ libs/all/Environment/Environment.js
@@ -5,25 +5,6 @@ function Environment(){}
 
 Environment.prototype.extend(GameObject.prototype).extend({
 	sprite: '', // path to sprite img to load
-
-	collision: function(gameObject) {
-		var topLeft = new Point(
-				this.position.x - this.width/2,
-				this.position.y + this.height/2
-			),
-			bottomRight = new Point(
-				this.position.x + this.width/2,
-				this.position.y - this.height/2
-			);
-		if (gameObject.isStationary) {
-			return gameObject.position.x + gameObject.width > this.position.x
-				&& gameObject.position.y + gameObject.height > this.position.y
-				&& gameObject.position.x < this.position.x + this.width
-				&& gameObject.position.y < this.position.y + this.height;
-		} else {
-			return gameObject.position.inside(topLeft, bottomRight);
-		}
-	}
 });
 
 module.exports = Environment;
--- libs/all/GameObject.js
+++ libs/all/GameObject.js
@@ -12,8 +12,23 @@ GameObject.prototype.extend({
 	width: 50,
 	height: 50,
 
-	collision: function(gameObj) {
-		console.log('test');
+	collision: function(gameObject) {
+		var topLeft = new Point(
+				this.position.x - this.width/2,
+				this.position.y + this.height/2
+			),
+			bottomRight = new Point(
+				this.position.x + this.width/2,
+				this.position.y - this.height/2
+			);
+		if (gameObject.isStationary) {
+			return gameObject.position.x + gameObject.width > this.position.x
+				&& gameObject.position.y + gameObject.height > this.position.y
+				&& gameObject.position.x < this.position.x + this.width
+				&& gameObject.position.y < this.position.y + this.height;
+		} else {
+			return gameObject.position.inside(topLeft, bottomRight);
+		}
 	}
 });
 
