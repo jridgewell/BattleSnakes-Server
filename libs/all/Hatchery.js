c6ef76eda538fff0df4fb07f796d3c8c926618ec
Justin Ridgewell <jridgewe@stetson.edu>
2012-11-11 20:58:50 -0500

    Make Hatchery use GameObject's collision()

--- libs/all/Hatchery.js
+++ libs/all/Hatchery.js
@@ -13,21 +13,6 @@ Hatchery.prototype.extend(GameObject.prototype).extend({
 	width: 250,
 	height: 250,
 	sprite: '', // path to sprite img to load
-
-	// Simple boundingbox algorithm
-	// TODO: Seems backwards, why woud a rock hit a snake?
-	// TODO: Souldn't we be calling collision on the snake? not the rock?
-	collision: function(gameObject) {
-		var topLeft = new Point(
-				this.position.x - this.width/2,
-				this.position.y + this.height/2
-			),
-			bottomRight = new Point(
-				this.position.x + this.width/2,
-				this.position.y - this.height/2
-			);
-		return gameObject.position.inside(topLeft, bottomRight);
-	}
 });
 
 module.exports = Hatchery;
