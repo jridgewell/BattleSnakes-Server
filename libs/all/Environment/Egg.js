37cd83436908710350c885af249d789ac4c44bd7
Justin Ridgewell <jridgewe@stetson.edu>
2012-11-19 16:07:37 -0500

    Check for snake collisions with eggs and hatcheries
    This will allow picking up and dropping off eggs.

--- libs/all/Environment/Egg.js
+++ libs/all/Environment/Egg.js
@@ -1,4 +1,5 @@
 var Environment = require('./Environment');
+var Point = require('../Point');
 
 /*
  * Color = {
@@ -9,17 +10,40 @@ var Environment = require('./Environment');
  */
 
 
-function Egg(hColor)
-{
+function Egg(hColor) {
 	// Constructor code here
 	// this.id = id;
 	this.type = this.constructor.name;
 	this.isCollidable = true;
 	this.hatchlingColor = hColor;
-
-
 }
 
-Egg.prototype.extend(Environment.prototype);
+Egg.prototype.extend(Environment.prototype).extend({
+	collision: function(gameObject) {
+		if (gameObject.id == this.id) {
+			return false;
+		}
+		var topLeft = new Point(
+				this.position.x - this.width/2,
+				this.position.y + this.height/2
+			),
+			bottomRight = new Point(
+				this.position.x + this.width/2,
+				this.position.y - this.height/2
+			);
+		if (gameObject.isStationary) {
+			return gameObject.position.x + gameObject.width >= this.position.x
+				&& gameObject.position.y + gameObject.height >= this.position.y
+				&& gameObject.position.x <= this.position.x + this.width
+				&& gameObject.position.y <= this.position.y + this.height;
+		} else {
+			var col = gameObject.position.inside(topLeft, bottomRight);
+			if (col) {
+				gameObject.pickUpEgg(this)
+			}
+			return col;
+		}
+	}
+});
 
 module.exports = Egg;
--- libs/all/Hatchery.js
+++ libs/all/Hatchery.js
@@ -13,6 +13,33 @@ Hatchery.prototype.extend(GameObject.prototype).extend({
 	width: 256,
 	height: 256,
 	sprite: '', // path to sprite img to load
+
+	collision: function(gameObject) {
+		if (gameObject.id == this.id) {
+			return;
+		}
+		var topLeft = new Point(
+				this.position.x - this.width/2,
+				this.position.y + this.height/2
+			),
+			bottomRight = new Point(
+				this.position.x + this.width/2,
+				this.position.y - this.height/2
+			);
+		if (gameObject.isStationary) {
+			return gameObject.position.x + gameObject.width >= this.position.x
+				&& gameObject.position.y + gameObject.height >= this.position.y
+				&& gameObject.position.x <= this.position.x + this.width
+				&& gameObject.position.y <= this.position.y + this.height;
+		} else {
+			var col = gameObject.position.inside(topLeft, bottomRight);
+			if (col && gameObject.hasEggs()) {
+				gameObject.dropOffEggs(this);
+			}
+			return col;
+		}
+	}
+
 });
 
 module.exports = Hatchery;
